ed401fe80ab2e809905b13380cd920b626c19a8b
Rahul Maurya <51026854+RahulMaurya9@users.noreply.github.com>
2022-06-30 12:54:50 +0530

    Time: 0 ms (100.00%), Space: 42.7 MB (64.33%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.java
+++ 206-reverse-linked-list/206-reverse-linked-list.java
@@ -7,6 +7,12 @@
  *     ListNode(int val) { this.val = val; }
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
+ Approach : handle 3 pointer
+	1. head
+	2. prev with null at start;
+	3. next 
+
+set next node like head.next and then set head pointer head.next = prev and then prev = head repeat unitl we find null
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
