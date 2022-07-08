2e02c33d5ff345a6c6a1818fad0625c67ade8169
Rahul Maurya <51026854+RahulMaurya9@users.noreply.github.com>
2022-07-08 08:00:21 +0530

    Time: 10 ms (5.20%), Space: 48.6 MB (7.29%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.java
+++ 2-add-two-numbers/2-add-two-numbers.java
@@ -10,18 +10,94 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode prev = new ListNode(0);
-        ListNode head = prev;
-        int carry = 0;
-        while(l1 != null || l2 != null || carry != 0){
-            ListNode cur = new ListNode(0);
-            int sum = ((l2 == null) ? 0 : l2.val) + ((l1 == null)? 0 : l1.val) + carry;
-            cur.val = sum% 10 ;
-            carry = sum /10;
-            prev.next = cur;
-            prev = cur;
-            l1 = l1== null ? null : l1.next;
-            l2  = l2 == null ? null : l2.next;
+        ListNode d3 = new ListNode();
+        ListNode d1 = l1 ;
+        ListNode d2 = l2;
+        ListNode head = d3; 
+        int flag = 0;
+        
+        while(d1 != null && d2 != null){
+            ListNode valAdd = new ListNode();
+            int sum = 0;
+            if(flag == 1){
+                sum = d1.val + d2.val +  1;
+            }else{
+                sum = d1.val + d2.val;
+            }
+            if( sum > 9){
+                valAdd.val = sum % 10;
+                d3.next = valAdd;
+                d3 = valAdd;
+                flag = 1;
+            }
+            else{
+                valAdd.val = sum;
+                d3.next = valAdd;
+                d3 = valAdd;
+                flag = 0;
+            }
+            d1 = d1.next;
+            d2 = d2.next;
+            
+        }
+        
+        if(d1 != null){
+            System.out.println("D1 have big list");
+            while(d1 != null){
+                System.out.println(d1.val);
+                ListNode data = new ListNode();
+                int sum= 0;
+                if(flag ==1 ){
+                    sum = d1.val + 1;
+                    
+                }else{
+                    sum = d1.val;
+                }
+                if(sum > 9 ){
+                    data.val = sum % 10;
+                    d3.next = data;
+                    d3 = data;
+                    flag = 1;
+                }else{
+                    data.val = sum;
+                    d3.next = data;
+                    d3 = data;
+                    flag = 0;
+                }
+                d1 = d1.next;
+            }
+            
+        }
+        if(d2 != null){
+            System.out.println("D2 have big list");
+            while(d2 != null){
+                ListNode data = new ListNode();
+                int sum= 0;
+                if(flag ==1 ){
+                    sum = d2.val + 1;
+                }else{
+                    sum = d2.val;
+                }
+                if(sum > 9 ){
+                    data.val = sum % 10;
+                    d3.next = data;
+                    d3 = data;
+                    flag = 1;
+                }else{
+                    data.val = sum;
+                    d3.next = data;
+                    d3 = data;
+                    flag = 0;
+                }
+                d2 = d2.next;
+            }
+        }
+        if(flag == 1){
+            ListNode lastnode = new ListNode();
+            lastnode.val = 1;
+            d3.next = lastnode;
+            d3 = lastnode;
+            
         }
         return head.next;
     }
