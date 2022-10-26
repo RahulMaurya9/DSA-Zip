8afb500b7bc9cc5cb11e28229b8eea2614f261b5
Rahul Maurya <51026854+RahulMaurya9@users.noreply.github.com>
2022-10-27 00:28:44 +0530

    Time: 250 ms (90.00%), Space: 113.7 MB (83.55%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -0,0 +1,22 @@
+#define deb(x) cout<<#x<<" "<<x<<endl;
+
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        if(nums.size()< 2) return false;
+        unordered_map<int, int> mp;
+        mp[0]=-1;
+        int runningsum= 0;
+        for(int i = 0 ; i<nums.size() ; i++){
+            runningsum += nums[i];
+            if(k != 0) runningsum %= k;
+            if(mp.find(runningsum) != mp.end()){
+                if(i-mp[runningsum] > 1) return true;
+            }else{
+                mp[runningsum] = i;
+            }
+            
+        }
+        return false;
+    }
+};
