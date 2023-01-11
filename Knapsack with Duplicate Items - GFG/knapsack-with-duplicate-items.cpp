37720f941a738757460f8c5fb65e26364c3a6164
Rahul Maurya <51026854+RahulMaurya9@users.noreply.github.com>
2023-01-12 00:59:21 +0530

    Added solution - LeetHub

--- Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.cpp
+++ Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.cpp
@@ -9,21 +9,33 @@ using namespace std;
 
 class Solution{
 public:
-    int MaxKnap(int ind , int weigh , int val[] , int wt[], vector<vector<int>>& dp){
-        if(ind == 0){
-            if(wt[0] <= weigh) return (weigh/wt[0])*val[0];
-            return 0;
-        }
-        if(dp[ind][weigh] != -1 ) return dp[ind][weigh];
-        int nottake = MaxKnap(ind-1 , weigh , val , wt, dp);
-        int take = INT_MIN;
-        if(wt[ind] <= weigh) take = val[ind] + MaxKnap(ind , weigh - wt[ind] , val , wt, dp);
-        return dp[ind][weigh] = max(nottake , take);
-    }
+    // int MaxKnap(int ind , int weigh , int val[] , int wt[], vector<vector<int>>& dp){
+    //     if(ind == 0){
+    //         if(wt[0] <= weigh) return (weigh/wt[0])*val[0];
+    //         return 0;
+    //     }
+    //     if(dp[ind][weigh] != -1 ) return dp[ind][weigh];
+    //     int nottake = MaxKnap(ind-1 , weigh , val , wt, dp);
+    //     int take = INT_MIN;
+    //     if(wt[ind] <= weigh) take = val[ind] + MaxKnap(ind , weigh - wt[ind] , val , wt, dp);
+    //     return dp[ind][weigh] = max(nottake , take);
+    // }
     int knapSack(int N, int W, int val[], int wt[])
     {
-        vector<vector<int>>dp(N , vector<int>(W+1 , -1));
-        return MaxKnap(N - 1 , W , val , wt, dp);
+        vector<vector<int>>dp(N , vector<int>(W+1 , 0));
+        // return MaxKnap(N - 1 , W , val , wt, dp);
+        for(int w =  0 ; w <= W ; ++w){
+            dp[0][w] =((int)(w/wt[0])) * val[0];
+        }
+        for(int ind = 1 ;  ind < N ; ++ind){
+            for(int weight  = 0 ; weight <= W ; ++weight){
+                int nottake = dp[ind-1][weight];
+                int take = 0; 
+                if(wt[ind] <= weight) take = val[ind] + dp[ind][weight - wt[ind]];
+                dp[ind][weight]= max(nottake , take);
+            }
+        }
+        return dp[N-1][W];
     }
 };
 
