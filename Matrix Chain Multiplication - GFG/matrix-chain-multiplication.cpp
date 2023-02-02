25056f6df02eaedb31d67df4d258956a99b8b827
Rahul Maurya <51026854+RahulMaurya9@users.noreply.github.com>
2023-02-02 11:26:06 +0530

    Added solution - LeetHub

--- Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
+++ Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
@@ -13,6 +13,7 @@ public:
         if(i == j) return 0;
         if(dp[i][j]  != -1) return dp[i][j];
         int mini = 1e9;
+        
         for(int k = i ; k < j ; k++){
             int steps = arr[i-1]* arr[k] * arr[j] + MCM(i,k,arr, dp) + MCM(k+1 , j , arr, dp);
             mini = steps< mini ? steps : mini;
@@ -23,8 +24,19 @@ public:
     int matrixMultiplication(int N, int arr[])
     {
         // code here
-        vector<vector<int>> dp(N , vector<int>(N,-1));
-        return MCM(1 , N-1 , arr, dp);
+        vector<vector<int>> dp(N , vector<int>(N,0));
+        //return MCM(1 , N-1 , arr, dp);
+        for(int i  = N-1 ; i >= 1; i--){
+            for(int  j = i + 1 ; j < N ; j++){
+                int mini = 1e9;
+                for(int k = i  ; k < j; k++){
+                    int steps = arr[i-1]* arr[k] * arr[j] + dp[i][k] + dp[k+1][j];
+                    mini = steps< mini ? steps : mini;
+                }
+                dp[i][j] = mini;
+            }
+        }
+        return dp[1][N-1];
     }
 };
 
